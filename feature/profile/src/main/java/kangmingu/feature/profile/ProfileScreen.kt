0961f5c023aa8f19ca0cff36d3e1c127e72396eb
kangmin1012 <kangmin1012@naver.com>
2025-06-20 15:15:24 +0900

    feat : 소개 영역 카드 추가

--- feature/profile/src/main/java/kangmingu/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/kangmingu/feature/profile/ProfileScreen.kt
@@ -2,11 +2,14 @@ package kangmingu.feature.profile
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -92,6 +95,23 @@ private fun ProfileContent(
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.onBackground
         )
+
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            colors = CardDefaults.cardColors().copy(
+                containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                text = profile.introduce,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
 
@@ -114,7 +134,8 @@ private class ProfilePreviewProvider : PreviewParameterProvider<Profile> {
                 imageUrl = "",
                 name = "Kang Min gu",
                 job = "Android",
-                location = "Seoul"
+                location = "Seoul",
+                introduce = "Introduce Yourself"
             )
         )
 }
